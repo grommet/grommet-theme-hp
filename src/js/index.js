92965cf7a646f6400d27b53a56ac9b25ff50d4cb
Eric Soderberg <eric.soderberg@hpe.com>
2019-03-12 15:24:28 -0700

    Simplify CheckBox check styling

--- src/js/index.js
+++ src/js/index.js
@@ -67,16 +67,4 @@ export const hp = deepFreeze({
       `}
     `,
   },
-  checkBox: {
-    icon: {
-      extend: css`
-        box-sizing: border-box;
-        position: absolute;
-        top: 0px;
-        left: 0px;
-        width: ${props => props.theme.checkBox.size};
-        height: ${props => props.theme.checkBox.size};
-      `,
-    },
-  },
 });
